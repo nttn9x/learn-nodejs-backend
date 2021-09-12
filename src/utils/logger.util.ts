6178b7dc43db9d0fb3d833655e56ed252a250675
Nguyen <nttn9x@gmail.com>
2021-09-12 21:01:29 +0700

    Log

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -3,15 +3,16 @@ import fs from "fs";
 import path from "path";
 import { format } from "date-fns";
 
-const nowStr = format(new Date(), "MM_dd_yyyy");
+const nowStr = format(new Date(), "yyyy_dd_MM");
 const fileName = path.join("src", "log", nowStr);
 
-fs.writeFileSync(path.join("src", "log", nowStr), "");
+if (!fs.existsSync(fileName)) {
+  fs.writeFileSync(fileName, "");
+}
 
 const logger = pino(
   {
     prettyPrint: true,
-    level: "trace",
   },
   pino.destination(fileName)
 );
